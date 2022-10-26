c1a5bbd06e4362999b7c0225cdd5581da47566de
Amirulmath <amirulmath760@gmail.com>
2022-10-27 02:12:28 +0600

    Web title and favicon add

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAw48DJAj9hyZqT0-WocI5bNnIFpliSbdI",
+  authDomain: "learning-computer-64f7b.firebaseapp.com",
+  projectId: "learning-computer-64f7b",
+  storageBucket: "learning-computer-64f7b.appspot.com",
+  messagingSenderId: "1061737578456",
+  appId: "1:1061737578456:web:794715768ed742d4ac7cad"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
