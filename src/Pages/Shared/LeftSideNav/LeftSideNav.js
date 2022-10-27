26c522622d3b5c373f9f0cbe7138693dfd03d0af
Amirulmath <amirulmath760@gmail.com>
2022-10-27 10:55:40 +0600

    Course lists API add to link

--- src/Pages/Shared/LeftSideNav/LeftSideNav.js
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftSideNav = () => {
     const [lists, setLists] = useState([]);
@@ -14,6 +15,13 @@ const LeftSideNav = () => {
     return (
         <div>
             <h2>List of Courses: {lists.length}</h2>
+            <div>
+                {
+                    lists.map(list => <p key={list.course_id}>
+                        <Link to={`/courseList/${list.course_id}`}>{list.course_name}</Link>
+                    </p>)
+                }
+            </div>
         </div>
     );
 };
