0bf6866d4c8fcad46bbbf5545745d14ec059f889
Amirulmath <amirulmath760@gmail.com>
2022-10-27 11:58:33 +0600

    React icons install & use font awesome

--- src/Pages/Shared/RightSideNav/RightSideNav.js
+++ src/Pages/Shared/RightSideNav/RightSideNav.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import { FaGithub, FaGoogle } from "react-icons/fa";
 
 const RightSideNav = () => {
     return (
         <div>
-            <h2>Right Side Nave</h2>
+            <div className="d-sm-grid d-lg-flex gap-4">
+                <Button variant="primary" size="lg">
+                    <FaGoogle></FaGoogle> Login with Google
+                </Button>
+                <Button variant="secondary" size="lg">
+                    <FaGithub></FaGithub> Login with Github
+                </Button>
+            </div>
         </div>
     );
 };
